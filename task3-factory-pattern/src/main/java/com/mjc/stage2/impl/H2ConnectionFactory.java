d1402b1a71a9afc1a9913cc39a193f9af523c348
Kirill Vlasovets <k.vlasovets@a1qa.com>
2024-02-05 02:12:59 +0400

    factory fixes added

--- task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
+++ task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
@@ -2,13 +2,25 @@ package com.mjc.stage2.impl;
 
 import com.mjc.stage2.ConnectionFactory;
 
+import java.io.IOException;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
 public class H2ConnectionFactory implements ConnectionFactory {
     @Override
     public Connection createConnection() {
-        Connection connection = null;
-        return null;
+        try {
+            Properties properties = new Properties();
+            properties.load(H2ConnectionFactory.class.getClassLoader().getResourceAsStream("h2database.properties"));
+            String url = properties.getProperty("db_url");
+            String user = properties.getProperty("user");
+            String password = properties.getProperty("password");
+            return DriverManager.getConnection(url, user, password);
+        } catch (IOException | SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
 
