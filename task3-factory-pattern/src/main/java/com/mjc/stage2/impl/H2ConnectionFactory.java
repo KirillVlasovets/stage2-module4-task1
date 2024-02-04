5c3e881015cffbb7964a3f7ef5caee2649546a57
Kirill Vlasovets <k.vlasovets@a1qa.com>
2024-02-05 01:40:43 +0400

    solution added

--- task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
+++ task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
@@ -1,6 +1,14 @@
 package com.mjc.stage2.impl;
 
-public class H2ConnectionFactory {
-    // Write your code here!
+import com.mjc.stage2.ConnectionFactory;
+
+import java.sql.Connection;
+
+public class H2ConnectionFactory implements ConnectionFactory {
+    @Override
+    public Connection createConnection() {
+        Connection connection = null;
+        return null;
+    }
 }
 
